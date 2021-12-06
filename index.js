e90cbd46ed511ceeb6322aa419045481b5b9abfc
Juan <ssjmatos@gmail.com>
2021-12-06 12:51:03 -0500

    loop lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name, occasion) {
+    let cards = []
+    for (let i = 0; i < name.length; i++) {
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${occasion} gift!`);
+  }
+
+  return cards;
+}
+
+function countDown(i){
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+    return i;
+}
